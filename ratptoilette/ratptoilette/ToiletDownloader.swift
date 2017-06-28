23aa1303e09bf2668188652799cf4720df7ad65b
ScNicolas <sirac.nicolas@gmail.com>
2017-06-28 11:57:47 +0200

    Toilet

--- ratptoilette/ratptoilette/ToiletDownloader.swift
+++ ratptoilette/ratptoilette/ToiletDownloader.swift
@@ -1,47 +1,41 @@
-//
-//  ToiletteRetriever.swift
-//  ratptoilette
-//
-//  Created by etudiant on 07/06/2017.
-//  Copyright © 2017 ESGI. All rights reserved.
-//
-
-iimport Foundation
+import Foundation
 
 class ToiletDownloader{
     
     class func load(url: URL) {
-        let localURL = Bundle.main.url(forResource: "toilets", withExtension: "json")
-
-
-        let sessionConfig = URLSessionConfiguration.default
-    let session = URLSession(configuration: sessionConfig)
-    var request = try! URLRequest(url: url)
-    request.httpMethod="GET"
-    let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
-        if let tempLocalUrl = tempLocalUrl, error == nil {
-            // Success
-            if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                print("Success: \(statusCode)")
-            }
-            
-            do {
-                try FileManager.default.copyItem(at: tempLocalUrl, to: localURL!)
-            } catch (let writeError) {
-                print("error writing file \(localURL) : \(writeError)")
-            }
-            
-        } else {
-            print("Failure: %@", error?.localizedDescription);
-        }
-    }
-        do{
-        let path = Bundle.main.path(forResource: "toilets", ofType: "json")
-        let text =  try String(contentsOfFile: path!)
-        print(text)}
-        catch{}
         
-    task.resume()
+        let documentsUrl:URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
+        let destinationFileUrl = documentsUrl.appendingPathComponent("toilet.json")
+        
+        //Create URL to the source file you want to download
+        let fileURL = url
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL)
+        
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Successfully downloaded. Status code: \(statusCode)")
+                }
+                
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                } catch (let writeError) {
+                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                }
+                
+            } else {
+                print("Error took place while downloading a file. Error description: %@", error?.localizedDescription);
+            }
+        }
+        
+
+        task.resume()
+        
     }
 
 }
